9abf87982be0b8c5187b345baf507661bffdc994
Your Name <your-email@example.com>
2023-09-20 17:33:37 +0300

    Fixed Cors issue

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ async function bootstrap() {
   const env = config();
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST']
+    // origin: '*',
+    origin: 'https://transcriber-io.onrender.com',
+    methods: ['GET', 'POST'],
   });
   await app.listen(8080);
 }
