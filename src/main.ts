dfb30d54e19d2710ad52e2fe4edcc4d7cf012ecb
Your Name <your-email@example.com>
2023-09-20 18:03:41 +0300

    Cors fix 3

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {config} from 'dotenv';
 
 async function bootstrap() {
-  const env = config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    // origin: '*',
+
+  // Set up CORS options
+  const corsOptions = {
     origin: 'https://transcriber-io.onrender.com',
-    methods: ['GET', 'POST'],
-  });
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
